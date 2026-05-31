1038f8d52799a729cfa666ea420bfc5e3f0f4c36
MawuenaKwao <itsenaofficial@gmail.com>
2026-05-31 03:42:51 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+it# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Kwao Mary Mawuena
+# STUDENT ID: 2425403915
+# Email: itsenaofficial@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Kwao Mary Mawuena, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints KMM initials in large stylized block letters using the format required
 
+    print("")
+    print("...KMM")
+    print("")
+
+    print("...KK        KK****...MM        MM****...MM        MM****...")
+    print("...KK      KK  ****...MMM      MMM****...MMM      MMM****...")
+    print("...KK    KK    ****...MMMM    MMMM****...MMMM    MMMM****...")
+    print("...KK  KK      ****...MM MM  MM MM****...MM MM  MM MM****...")
+    print("...KKKK        ****...MM  MMMM  MM****...MM  MMMM  MM****...")
+    print("...KK  KK      ****...MM   MM   MM****...MM   MM   MM****...")
+    print("...KK    KK    ****...MM        MM****...MM        MM****...")
+    print("...KK      KK  ****...MM        MM****...MM        MM****...")
+    print("...KK        KK****...MM        MM****...MM        MM****...")
+    print("...KK        KK****...MM        MM****...MM        MM****...")
+
+    print("")
 
 
 main()
